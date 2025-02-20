587dc1ad6a65d796a54076726201661b7ffb5afe
Reinhard <rkugler@sba-research.org>
2025-02-20 20:09:26 +0000

    now it runs in docker

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 
 
 
-docker run --init -ti -p 222:222 potato2
+docker run --init \
+	   -ti \
+	   -p 222:222 \
+	   --cap-add=SYS_ADMIN \
+	   --security-opt seccomp=unconfined \
+	   --security-opt apparmor=unconfined \
+	   potato2
+
