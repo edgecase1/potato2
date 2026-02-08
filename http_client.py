556d69987590323bb92d796c02797b9b41e2843b
Reinhard <rkugler@sba-research.org>
2026-02-08 15:39:25 -0500

    updated the http client

--- http_client.py
+++ http_client.py
@@ -2,44 +2,48 @@ import requests
 
 BASE_URL = "http://localhost"
 
-def login(username, password):
-    print("[*] Logging in...")
-    data = {
-        "username": username,
-        "password": password
-    }
-    response = requests.post(f"{BASE_URL}/api/login", data=data)
-    
-    if response.status_code == 200:
-        session_id = response.text.strip()
-        print(f"[+] Login successful. Session ID: {session_id}")
-        return session_id
-    else:
-        print(f"[-] Login failed: {response.status_code}")
-        print(response.text)
-        return None
+class PotatoClient:
 
-def run_command(session_id, command):
-    print(f"[*] Running command: {command}")
-    data = {
-        "session_id": session_id,
-        "command": command
-    }
-    response = requests.post(f"{BASE_URL}/api/run", data=data)
-    
-    if response.status_code == 200:
-        print("[+] Command output:")
-        print(response.text)
-    else:
-        print(f"[-] Command failed: {response.status_code}")
-        print(response.text)
+    def login(self, username, password):
+        self.s = requests.Session()
 
+        print("[*] Logging in...")
+        data = {
+            "username": username,
+            "password": password
+        }
+
+        response = self.s.post(f"{BASE_URL}/api/login", data=data)
+        
+        if response.status_code == 200:
+            print(f"[+] Login successful.")
+            print(f"session value is {self.s.cookies['session']}")
+            return True
+        else:
+            print(f"[-] Login failed: {response.status_code}")
+            print(response.text)
+            return False
+    
+    def run_command(self, command):
+        print(f"[*] Running command: {command}")
+        data = {
+            "command": command
+        }
+        response = self.s.post(f"{BASE_URL}/api/run", data=data)
+        
+        if response.status_code == 200:
+            print("[+] Command output:")
+            print(response.text)
+        else:
+            print(f"[-] Command failed: {response.status_code}")
+            print(response.text)
+    
 if __name__ == "__main__":
     # Change credentials and command as needed
-    session = login("user", "XXX")
-    session = login("peter", "12345")
-    if session:
-        run_command(session, "uname -a")
-        run_command(session, "id")
-        run_command(session, "ps faux")
-        run_command(session, "ls -la /")
+    #session = login("user", "XXX")
+    c = PotatoClient()
+    if c.login("peter", "12345"):
+        c.run_command("uname -a")
+        c.run_command("id")
+        #c.run_command("ps faux")
+        #c.run_command("ls -la /")
